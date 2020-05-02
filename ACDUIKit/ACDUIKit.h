c8ddde3e58233e766db84ea27ba81ab1d0c82da8
jaeyoung <hideyf@gmail.com>
2020-05-02 14:46:17 +0900

    initial commit

---
+++ ACDUIKit/ACDUIKit.h
@@ -0,0 +1,19 @@
+//
+//  ACDUIKit.h
+//  ACDUIKit
+//
+//  Created by jaeyoung on 2020/05/02.
+//  Copyright © 2020 Appcid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ACDUIKit.
+FOUNDATION_EXPORT double ACDUIKitVersionNumber;
+
+//! Project version string for ACDUIKit.
+FOUNDATION_EXPORT const unsigned char ACDUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACDUIKit/PublicHeader.h>
+
+
